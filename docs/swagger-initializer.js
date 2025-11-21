ad91fe85df0eb4771a52de112a2e06924b21560b
tebarek <tebarek29@gmail.com>
2025-11-21 23:10:08 +0300

    edited the doc

--- docs/swagger-initializer.js
+++ docs/swagger-initializer.js
@@ -1,22 +1,17 @@
-window.onload = function() {
-  //<editor-fold desc="Changeable Configuration Block">
+window.onload = function () {
+  //<editor-fold desc="Changeable Configuration Block">
 
- // the following lines will be replaced by docker/configurator, when it runs in a docker-container
-  window.ui = SwaggerUIBundle({
-    // 👇 CRITICAL CHANGE: This tells Swagger UI to look for your specification file 
+  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+  window.ui = SwaggerUIBundle({
+    // 👇 CRITICAL CHANGE: This tells Swagger UI to look for your specification file
     // in the same directory (the /docs folder).
-    url: "./openapi.yaml", 
-    dom_id: '#swagger-ui',
-    deepLinking: true,
-    presets: [
-      SwaggerUIBundle.presets.apis,
-      SwaggerUIStandalonePreset
-    ],
-    plugins: [
-      SwaggerUIBundle.plugins.DownloadUrl
-    ],
-    layout: "StandaloneLayout"
-  });
+    url: "./openapi.yml",
+    dom_id: "#swagger-ui",
+    deepLinking: true,
+    presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
+    plugins: [SwaggerUIBundle.plugins.DownloadUrl],
+    layout: "StandaloneLayout",
+  });
 
-//</editor-fold>
+  //</editor-fold>
 };
